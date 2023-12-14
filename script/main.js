0b22d1ca712f770a9353e12e1df99d35f457964d
knollmilena <50546237+knollmilena@users.noreply.github.com>
2023-12-14 15:09:54 +0300

    1. added tracking of changes to js, ​​njk, css files. 2. added pages /login and /singup

--- script/main.js
+++ script/main.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { nanoid } = require("nanoid");
 const bcrypt = require("bcryptjs");
-const router = express.Router();
+const routerLogin = express.Router();
+const routerSignUp = express.Router();
 const app = express();
 
 nunjucks.configure("src", {
@@ -21,6 +22,7 @@ nunjucks.configure("src", {
     commentEnd: "#]",
   },
 });
+
 app.set("view engine", "njk");
 app.use(cookieParser());
 
@@ -38,9 +40,28 @@ app.get("/", (req, res) => {
 });
 
 // npx kill-port 3000
-app.use("/login", router);
-router.get("/", (req, res) => {
+const auth = () => async (req, res, next) => {
+  if (!req.cookies["sessionId"]) {
+    return next();
+  }
+  const user = await findUserBySessionId(req.db, req.cookies["sessionId"]);
+  req.user = user;
+  req.sessionId = req.cookies["sessionId"];
+  next();
+};
+
+app.use("/login", routerLogin);
+routerLogin.get("/", auth(), (req, res) => {
   res.render("login", {
+    user: req.user,
+    authError: req.query.authError === "true",
+  });
+});
+
+app.use("/signup", routerSignUp);
+
+routerSignUp.get("/", (req, res) => {
+  res.render("signup", {
     user: req.body.id,
   });
 });
@@ -48,4 +69,69 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`  Listening on http://localhost:${port}`);
+});
+const { MongoClient, ObjectId } = require("mongodb");
+const clientPromise = MongoClient.connect("mongodb://localhost:27022", function (err, database) {
+  if (err) {
+    return console.log(err);
+  }
+  db = database;
+});
+
+app.use(async (req, res, next) => {
+  try {
+    const client = await clientPromise;
+    req.db = client.db("users");
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const createUser = async (db, username, password) => {
+  const id = ObjectId();
+  password = bcrypt.hashSync(password, 7);
+  await db.collection("users").insertOne({ _id: ObjectId(id), username, password });
+  return id;
+};
+
+const findUserBySessionId = async (db, sessionId) => {
+  const session = await db.collection("sessions").findOne(
+    { sessionId },
+    {
+      projection: { userId: 1 },
+    }
+  );
+
+  if (!session) {
+    return;
+  }
+  return db.collection("users").findOne({ _id: ObjectId(session.userId) });
+};
+
+const findUserByUsername = async (db, username) => db.collection("users").findOne({ username });
+
+const createSession = async (db, userId) => {
+  const sessionId = nanoid();
+
+  await db.collection("sessions").insertOne({
+    userId,
+    sessionId,
+  });
+
+  return sessionId;
+};
+
+async function deleteSession(db, sessionId) {
+  await db.collection("sessions").deleteOne({ sessionId });
+}
+
+routerLogin.post("/login", bodyParser.urlencoded({ extended: false }), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserByUsername(req.db, username);
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.redirect("/?authError=true");
+  }
+  const sessionId = await createSession(req.db, user._id);
+  res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
 });
